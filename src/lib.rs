85896494f5b0e45b26551388a3896dcc39a284a3
RustyRobot <rustfmtbot@yandex.ru>
2018-12-03 00:32:55 +0300

    rustyrobot formatting

--- src/lib.rs
+++ src/lib.rs
@@ -2,26 +2,24 @@ use std::ops::Range;
 
 #[derive(Clone, Hash, Eq, PartialEq)]
 pub struct NonemptyRange {
-  pub start: usize,
-  pub size_minus_one: usize,
+    pub start: usize,
+    pub size_minus_one: usize,
 }
 
 impl IntoIterator for NonemptyRange {
-  type Item = usize;
-  type IntoIter = Range<usize>;
-  fn into_iter(self) -> Self::IntoIter {
-    self.start..self.size_minus_one + self.start + 1
-  }
+    type Item = usize;
+    type IntoIter = Range<usize>;
+    fn into_iter(self) -> Self::IntoIter {
+        self.start..self.size_minus_one + self.start + 1
+    }
 }
 
 #[test]
 fn test_nonempty_range_into_iterator() {
-  assert!(
-    NonemptyRange {
-      start: 5,
-      size_minus_one: 2
+    assert!(NonemptyRange {
+        start: 5,
+        size_minus_one: 2
     }
     .into_iter()
-    .eq([5, 6, 7].into_iter().cloned())
-  )
+    .eq([5, 6, 7].into_iter().cloned()))
 }
